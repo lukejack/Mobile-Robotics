ab4954a0258c169f364a42914af4b5aa4496ad5a
Luke Jackson <jacksonluke96@yahoo.co.uk>
2017-03-09 12:35:45 +0000

    Improved colour ranges, post proximity, obstruction avoidance
    Commented code better

--- assignment.py
+++ assignment.py
@@ -28,13 +28,13 @@ class Follower:
     self.image_height = 480
     self.seen_angular = 0.3
     self.search_angular = 0.8
-    self.forward_velocity = 0.7
+    self.forward_velocity = 1
     
     #Colour slicing
     self.colours = {'red': {'found': False, 'min': numpy.array((0, 0, 1)), 'max': numpy.array((0, 0, 255))},
-                    'blue': {'found': False, 'min': numpy.array((1, 0, 0)), 'max': numpy.array((255, 0, 0))},
-                    'yellow': {'found': False, 'min': numpy.array((100, 100, 0)), 'max': numpy.array((255, 255, 0))},
-                    'green': {'found': False, 'min': numpy.array((0, 1, 0)), 'max': numpy.array((0, 255, 0))}}
+                    'blue': {'found': False, 'min': numpy.array((100, 0, 0)), 'max': numpy.array((255, 5, 5))},
+                    'yellow': {'found': False, 'min': numpy.array((0, 100, 100)), 'max': numpy.array((0, 255, 255))},
+                    'green': {'found': False, 'min': numpy.array((0, 10, 0)), 'max': numpy.array((0, 255, 0))}}
         
     self.red_found = False
     self.blue_found = False
@@ -50,65 +50,124 @@ class Follower:
     self.scan_data = None
     self.max_depth = 0
     
-    #Modes: 0 - spin_search, 1 - move_found, 2 - move_search
+    #Modes: 0 - spin_search, 1 - move_found, 2 - move_search, 3 - avoid obstruction
     self.mode = 1
     self.timer = 0
+    self.obstruction_timer = 0
     
-  def main(self, bgr):
-      #Get moments by colour slicing
-      coords = []
-      for key, value in self.colours.iteritems():
-          if (value['found'] == False):
-              coords.append([key, cv2.moments(cv2.inRange(bgr, value['min'], value['max']))])
+  def main(self, image):
       
-      #Check for each colour post
-      for colour in coords:
-          #If object seen
-          if colour[1]['m00'] > 0:
-              self.max_depth = 0
-              self.mode = 1
-              cx = int(colour[1]['m10']/colour[1]['m00'])
-              if (self.scan_data[320] < 0.8):
-                  print 'Found ', colour[0], '!'
-                  self.colours[colour[0]]['found'] = True
-              if (cx < self.image_width/2):
-                  self.twist.angular.z = self.seen_angular
-              else:
-                  self.twist.angular.z = -self.seen_angular
-              self.twist.linear.x = self.forward_velocity
-              self.cmd_vel_pub.publish(self.twist)
-              break
-          
-          #Nothing visible
-          else:
-              #If there are none of any colour
-              if (colour[0] == coords[-1][0]):
-                  if (self.mode == 2):
-                      self.twist.linear.x = self.forward_velocity
-                      self.twist.angular.z = 0
-                      if (self.scan_data[320] < 1):
-                          self.mode = 0
+      #Obstruction avoidance mode
+      if (self.mode == 3):
+          #Initialise timer
+          if (self.obstruction_timer == 0):
+              self.obstruction_timer = self.time.time()
+          #Timed movements to adjust position
+          if ((self.time.time() - self.obstruction_timer) > 0.5):
+              if ((self.time.time() - self.obstruction_timer) > 1):
+                  if ((self.time.time() - self.obstruction_timer) > 1.5):
+                      if ((self.time.time() - self.obstruction_timer) > 2):
+                          print 'Correction: Finished'
+                          self.twist.linear.x = 0
+                          self.twist.angular.x = 0
+                          self.mode = 1
+                          self.obstruction_timer = 0
                       else:
-                          self.cmd_vel_pub.publish(self.twist)
+                          print 'Correction: Turning back'
+                          self.twist.linear.x = 0
+                          self.twist.angular.x = -self.search_angular
                   else:
-                      if (self.mode != 0):
-                          self.max_depth = 0
-                          self.mode = 0
-                          self.timer = self.time.time()
-                      if ((self.mode == 0) & ((self.time.time() - self.timer) > 1.6 * numpy.pi + 1)):
-                          if (self.scan_data[320] > self.max_depth - 0.3):
-                              #START TIMER
-                              self.mode = 2
-                          #go to deepest point
-                          
-                      self.twist.linear.x = 0
-                      self.twist.angular.z = self.search_angular
+                      print 'Correction: Moving forwards'
+                      self.twist.angular.x = 0
+                      self.twist.linear.x = self.forward_velocity
+              else:
+                  print 'Correction: Turning sideways'
+                  self.twist.linear.x = 0
+                  self.twist.angular.x = self.search_angular
+          else:
+              print 'Correction: Moving backwards'
+              self.twist.linear.x = -self.forward_velocity
+          self.cmd_vel_pub.publish(self.twist)
+          
+      #No obstruction avoidance
+      else:
+          #Get moments by colour slicing
+          coords = []
+          for key, value in self.colours.iteritems():
+              if (value['found'] == False):
+                  mask = cv2.inRange(image, value['min'], value['max'])
+                  coords.append([key, cv2.moments(mask), mask])
+          
+          #Check for each colour post
+          for colour in coords:
+              #If object seen
+              if colour[1]['m00'] > 0:
+                  #Reset depth watcher
+                  self.max_depth = 0
+                  self.mode = 1
+                  
+                  #Center point of cylinder
+                  cx = int(colour[1]['m10']/colour[1]['m00'])
+                              
+                  #If close to the colour
+                  if (colour[1]['m00'] > 13000000):
+                      print 'Found ', colour[0], '!'
+                      self.colours[colour[0]]['found'] = True
+                  else:
+                      if (self.obstruction()):
+                          print "I'm obstructed"
                       
-                      self.cmd_vel_pub.publish(self.twist)
+                  #Move to the lef / right
+                  self.twist.angular.z =  -float(cx - self.image_width / 2) / 100 
+                  #Move forwards
+                  self.twist.linear.x = self.forward_velocity
+                  self.cmd_vel_pub.publish(self.twist)
+                  break
+              
+              #This colour not visible
+              else:
+                  #If there are none of any colour
+                  if (colour[0] == coords[-1][0]):
+                      #If moving to a new search place
+                      if (self.mode == 2):
+                          
+                          self.twist.linear.x = self.forward_velocity
+                          self.twist.angular.z = 0
+                          #If has an obstruction
+                          if (self.obstruction()):
+                              #Start searching again
+                              self.twist.linear.x = 0
+                              self.mode = 1
+                              self.max_depth = 0
+                              break
+                          else:
+                              #Otherwise keep moving
+                              self.cmd_vel_pub.publish(self.twist)
+                      #Start finding max depth in circle
+                      else:
+                          if (self.mode != 0):
+                              #Initialise timer
+                              self.max_depth = 0
+                              self.mode = 0
+                              self.timer = self.time.time()
+                          #If a full revolution has occured
+                          if ((self.mode == 0) & ((self.time.time() - self.timer) > 1.6 * numpy.pi + 1)):
+                             #If looking at deepest path
+                             if (self.scan_data[320] > (self.max_depth - 0.4)):
+                                  #If obstructed, avoid obstruction
+                                  if (self.obstruction()):
+                                      self.max_depth = 0
+                                      self.mode = 3
+                                  #Otherwise move in that direction with mode 2
+                                  else:
+                                      self.max_depth = 0
+                                      self.mode = 2
+                          #Turn on the spot
+                          self.twist.linear.x = 0
+                          self.twist.angular.z = self.search_angular
+                          self.cmd_vel_pub.publish(self.twist)
 
-      print "Mode: ", self.mode
-      print "Max_depth: ", self.max_depth
-      cv2.imshow("window", bgr)
+      cv2.imshow("window", image)
       cv2.waitKey(3) 
   
   
@@ -116,11 +175,16 @@ class Follower:
       self.scan_data = data.ranges
       if ((self.mode == 0) & (data.ranges[320] > self.max_depth)):
           self.max_depth = data.ranges[320]
+          
+  def obstruction(self):
+      for p in range(50, 429):
+         if (self.scan_data[p] < 1):
+             return True
+      return False
       
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
     self.main(image)
-
 
 rospy.init_node('follower')
 follower = Follower()
